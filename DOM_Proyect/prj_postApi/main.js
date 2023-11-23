3fd4b413e3dca4814e1c1fc92f8786596c85cb60
salva_robles <118806717+Salvarobles@users.noreply.github.com>
2023-11-23 20:40:42 +0100

    cru delete

--- DOM_Proyect/prj_postApi/main.js
+++ DOM_Proyect/prj_postApi/main.js
@@ -36,30 +36,91 @@ function init() {
         id: null,
         title: titlePost.value,
         post: contentPost.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const postArray = [];
+        postArray.push(data);
+        renderPost(postList, postArray);
       })
-    })
-    .then(response => response.json())
-    .then(data => {
-      const postArray = [];
-      postArray.push(data);
-      renderPost(postList, postArray);
-    })
-    .catch(err => {
-      throw new Error("Error al hacer el post");
-    })
+      .catch((err) => {
+        throw new Error("Error al hacer el post");
+      });
+
+    titlePost.value = "";
+    contentPost.value = "";
   });
 }
 
 postList.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(e.target.id);
-  let editBtnPress = e.target.id = "edit-post";
-  let deleteBtnPress = e.target.id = "delete-post";
 
-  console.log(e.target.parentElement.dataset.id);
+  let editBtnPress = e.target.id == "edit-post";
+  let deleteBtnPress = e.target.id == "delete-post";
+  const postId = e.target.parentElement.dataset.id;
 
-})
+  const idURL = `http://localhost:4000/posts/${postId}`;
 
+  // Editar card
+  if (editBtnPress) {
+    e.preventDefault();
+
+    fetch(idURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        titlePost.value = data.title;
+        contentPost.value = data.post;
+      });
+
+      addPostForm.getAttribute("submit", "Editar Card");
+
+    
+
+    fetch(idURL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: titlePost.value,
+        post: contentPost.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {})
+      .catch((err) => {
+        throw new Error("Error al editar el post");
+      });
+  }
+
+  // Eliminar Card
+
+  if (deleteBtnPress) {
+    e.preventDefault();
+    fetch(idURL, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const postCard = e.target.closest(".card");
+        if (postCard) {
+          postCard.remove();
+        }
+      })
+      .catch((err) => {
+        throw new Error("Error al eliminar el post");
+      });
+  }
+});
 
 //---------------- Inicializar ---------------
 document.addEventListener("DOMContentLoaded", init);
